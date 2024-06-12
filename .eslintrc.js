7e289262c563a4b1701aeca850ec2ebe2ead11b9
abdulrouf-al <naruto-10-10@hotmail.com>
2024-06-12 15:37:13 +0300

    'no-console': 'off',

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  rules: {
+    'no-console': 'off',
+  },
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
